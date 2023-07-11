b53c69adbcd35a7ca52752887b3cab6848ede05d
Susan Zhou <szhou089@gmail.com>
2023-07-10 18:35:35 -0700

    adding getters/setters for attributes from Song class

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,4 @@
-import java.util.UUID;
+import java.util.*;
 
 public class Song {
     private String _id;
@@ -17,4 +17,50 @@ public class Song {
         _id = UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
 
+
+    public String getId() {
+        return this._id;
+    }
+
+    public void setTitle(String title) {
+        this._title = title;
+    }
+
+    public String getTitle() {
+        return this._title;
+    }
+
+    public void setArtist(String artist) {
+        this._artist = artist;
+    }
+    public String getArtist() {
+        return this._artist;
+    }
+
+    public void setDuration(String duration) {
+        if (!durationValidator(duration)) {
+            throw new IllegalArgumentException("Invalid time duration of song");
+        }
+        this._duration = duration;
+    }
+
+    public String getDuration() {
+        return this._duration;
+    }
+
+    public void setAlbum(String album) {
+        this._album = album;
+    }
+
+    public String getAlbum() {
+        return this._album;
+    }
+
+    public boolean durationValidator(String inputStr) {
+        if (inputStr == null || inputStr.isEmpty()) {
+            return false;
+        }
+        String regex = "^(?:[0-9]+(?:[:][0-9]+)?|[:][0-9]+)$";
+        return inputStr.matches(regex) ? true : false;
+    }
 }
