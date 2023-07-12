d68f94e59852baadcfb7e992791e3b0d8cf1a1ed
Susan Zhou <szhou089@gmail.com>
2023-07-12 09:07:00 -0700

    adding method that search and displays song detail if exists

--- src/PlayList.java
+++ src/PlayList.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class PlayList {
 
     private ArrayList<Song> songList;
+    private int trackNumber;
     private int playListSize;
 
     public PlayList () {
         this.songList = new ArrayList<Song>();
+        this.trackNumber = 0;
     }
 
     public int getPlayListSize() {
@@ -15,22 +17,24 @@ public class PlayList {
 
     public void addSong(Song song) {
         for (Song s : songList) {
-            if (s.getTitle().equals(song.getTitle())) {
-                System.out.printf("This song, %s is already in your playlist", s.getTitle());
+            if (s.getTitle().equals(song.getTitle()) && s.getArtist().equals(song.getArtist())) {
+                System.out.printf("This song, %s is already in your playlist\n", s.getTitle());
                 return;
             }
         }
         this.songList.add(song);
+        this.trackNumber++;
     }
 
     public Song findSong(String title) {
         for (Song s : songList) {
             if (s.getTitle().equals(title)) {
-                System.out.println(s.toString());
+                System.out.println("Track Number: " + (this.trackNumber-1) + " ," + s.toString());
                 return s;
             }
         }
 
         return null;
     }
+
 }
